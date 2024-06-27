8ff0d01abcf272bcc6ab744b4903672b70be0556
Matěj Šťastný <matysta@outlook.com>
2024-06-26 19:41:24 -0700

    Fixed a jump bug
    Fixed a bug, where you can jump even if you're not on the ground.

--- scripts/player.gd
+++ scripts/player.gd
@@ -15,7 +15,7 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("jump"): # and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get input direction: -1, 0, 1
